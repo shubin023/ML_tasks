658ceab770fb1999073a2b52c54c92faed68a010
shubin023 <antshubin023@gmail.com>
2024-12-07 17:02:03 +0300

    the first task - K-mean

--- Homeworks/Homework_2/task.py
+++ Homeworks/Homework_2/task.py
@@ -19,7 +19,7 @@ class KMeans:
         init : str
             Способ инициализации кластеров. Один из трех вариантов:
             1. random --- центроиды кластеров являются случайными точками,
-            2. sample --- центроиды кластеров выбираются случайно из  X,
+            2. sample --- центроиды кластеров выбираются случайно из X,
             3. k-means++ --- центроиды кластеров инициализируются 
                 при помощи метода K-means++.
         max_iter : int
@@ -27,8 +27,21 @@ class KMeans:
         
         """
         
-        pass
-        
+        self.k = n_clusters
+        self.init_type = init
+        self.max_iter = max_iter
+        self.X = None
+        self.centers = None
+        self.clusters = None
+
+    def re_init(self, init_type='random', dist_arr=None):
+        if init_type == 'random':
+            return np.random.uniform(self.X.min(axis=0), self.X.max(axis=0))
+        elif init_type == 'sample':
+            return self.X[np.random.choice(self.X.shape[0], replace=False)]
+        elif init_type == 'k-means++':
+            return self.X[np.random.choice(self.X.shape[0], p=dist_arr / dist_arr.sum())]
+
     def fit(self, X: np.array, y = None) -> NoReturn:
         """
         Ищет и запоминает в self.centroids центроиды кластеров для X.
@@ -43,7 +56,52 @@ class KMeans:
             параметры X и y, даже если y не используется).
         
         """
-        pass
+        self.X = X
+
+        if self.init_type == 'random':
+            self.centers = np.random.uniform(self.X.min(axis=0), self.X.min(axis=0), size=(self.k, self.X.shape[1]))
+
+        elif self.init_type == 'sample':
+            unique_ids = np.random.choice(self.X.shape[0], self.k, replace=False)
+            self.centers = self.X[unique_ids]
+
+        elif self.init_type == 'k-means++':
+            centers_ind = []
+            first_ind = np.random.randint(0, self.X.shape[0] - 1)
+            centers_ind.append(first_ind)
+            dist_arr_sqr = np.linalg.norm(self.X[first_ind] - self.X, axis=1) ** 2
+            for _ in range(self.k - 1):
+                rand_ind = np.random.choice(self.X.shape[0], p=dist_arr_sqr / dist_arr_sqr.sum())
+                centers_ind.append(rand_ind)
+                dist_arr_new = np.linalg.norm(self.X[rand_ind] - self.X, axis=1) ** 2
+                dist_arr_sqr = np.minimum(dist_arr_sqr, dist_arr_new)
+            self.centers = self.X[centers_ind]
+
+        self.clusters = np.zeros(self.X.shape[0], dtype=int)
+
+        for _ in range(self.max_iter):
+            dist = np.array([np.inf] * self.X.shape[0])
+            for clust_ind, center in enumerate(self.centers):
+                dist_arr = np.linalg.norm(self.X - center, axis=1)
+                change_ind = dist_arr < dist
+                dist[change_ind] = dist_arr[change_ind]
+                self.clusters[change_ind] = clust_ind
+
+            new_centers = np.array([self.X[self.clusters == i].mean(axis=0) if len(self.X[self.clusters == i]) > 0 else self.re_init(self.init_type, dist)
+                                     for i in range(self.k)])
+
+            if np.allclose(self.centers, new_centers, atol=1e-4):
+                break
+            self.centers = new_centers
+
+            # old_centers = self.centers.copy()
+            #
+            # for cluster in range(self.k):
+            #     self.centers[cluster] = self.X[self.clusters == cluster].mean(axis=0)
+            # if sum(old_centers == self.centers) == self.k:
+            #     break
+
+
     
     def predict(self, X: np.array) -> np.array:
         """
@@ -62,7 +120,15 @@ class KMeans:
             (по одному индексу для каждого элемента из X).
         
         """
-        pass
+        answer = np.zeros(X.shape[0], dtype=int)
+        dist = np.array([np.inf] * X.shape[0])
+        for clust_ind, center in enumerate(self.centers):
+            dist_arr = np.linalg.norm(center - X, axis=1)
+            change_ind = dist_arr < dist
+            dist[change_ind] = dist_arr[change_ind]
+            answer[change_ind] = clust_ind
+
+        return np.array(answer)
     
 # Task 2
 
