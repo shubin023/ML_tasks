715e633ada0aad1bc929d81433ad15c7fdefe9aa
shubin023 <antshubin023@gmail.com>
2025-01-10 05:23:01 +0300

    HW 4, task 1

---
+++ Homeworks/Homework_4/task.py
@@ -0,0 +1,157 @@
+import numpy as np
+import copy
+from typing import NoReturn
+
+
+# Task 1
+
+class Perceptron:
+    def __init__(self, iterations: int = 100):
+        """
+        Parameters
+        ----------
+        iterations : int
+        Количество итераций обучения перцептрона.
+
+        Attributes
+        ----------
+        w : np.ndarray
+        Веса перцептрона размерности X.shape[1] + 1 (X --- данные для обучения), 
+        w[0] должен соответстовать константе, 
+        w[1:] - коэффициентам компонент элемента X.
+
+        Notes
+        -----
+        Вы можете добавлять свои поля в класс.
+        
+        """
+
+        self.w = None
+        self.iterations = iterations
+    
+    def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+        """
+        Обучает простой перцептрон. 
+        Для этого сначала инициализирует веса перцептрона,
+        а затем обновляет их в течении iterations итераций.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, на котором обучается перцептрон.
+        y: np.ndarray
+            Набор меток классов для данных.
+        
+        """
+        y[y == np.unique(y)[0]] = 0
+        y[y == np.unique(y)[1]] = 1
+        X_new = np.column_stack((np.array([1] * X.shape[0]), X))
+        self.w = np.array([0] * X_new.shape[1])
+        for _ in range(self.iterations):
+            wrong_labels = np.sign(self.w @ X_new.T) != y
+            self.w = self.w + np.sum(y[wrong_labels].reshape((-1, 1)) * X_new[wrong_labels], axis=0)
+            
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Предсказывает метки классов.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, для которого необходимо вернуть метки классов.
+        
+        Return
+        ------
+        labels : np.ndarray
+            Вектор индексов классов 
+            (по одной метке для каждого элемента из X).
+        
+        """
+        X_new = np.column_stack((np.array([1] * X.shape[0]), X))
+        answer = np.sign(self.w @ X_new.T)
+        answer[answer == -1] = 0
+        return answer
+    
+# Task 2
+
+class PerceptronBest:
+
+    def __init__(self, iterations: int = 100):
+        """
+        Parameters
+        ----------
+        iterations : int
+        Количество итераций обучения перцептрона.
+
+        Attributes
+        ----------
+        w : np.ndarray
+        Веса перцептрона размерности X.shape[1] + 1 (X --- данные для обучения), 
+        w[0] должен соответстовать константе, 
+        w[1:] - коэффициентам компонент элемента X.
+
+        Notes
+        -----
+        Вы можете добавлять свои поля в класс.
+        
+        """
+
+        self.w = None
+    
+    def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+        """
+        Обучает перцептрон.
+
+        Для этого сначала инициализирует веса перцептрона, 
+        а затем обновляет их в течении iterations итераций.
+
+        При этом в конце обучения оставляет веса, 
+        при которых значение accuracy было наибольшим.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, на котором обучается перцептрон.
+        y: np.ndarray
+            Набор меток классов для данных.
+        
+        """
+        pass
+            
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Предсказывает метки классов.
+        
+        Parameters
+        ----------
+        X : np.ndarray
+            Набор данных, для которого необходимо вернуть метки классов.
+        
+        Return
+        ------
+        labels : np.ndarray
+            Вектор индексов классов 
+            (по одной метке для каждого элемента из X).
+        
+        """
+        pass
+    
+# Task 3
+
+def transform_images(images: np.ndarray) -> np.ndarray:
+    """
+    Переводит каждое изображение в вектор из двух элементов.
+        
+    Parameters
+    ----------
+    images : np.ndarray
+        Трехмерная матрица с черное-белыми изображениями.
+        Её размерность: (n_images, image_height, image_width).
+
+    Return
+    ------
+    np.ndarray
+        Двумерная матрица с преобразованными изображениями.
+        Её размерность: (n_images, 2).
+    """
+    return np.zeros((images.shape[0], 2))
